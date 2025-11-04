6e85242f9524236d2c7641928b977b23ff88154a
Talha23542773 <23-54277-3@student.aiub.edu>
2025-11-04 17:20:31 +0600

    23-54277-3

---
+++ Project/MortgageCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MortgageCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Loan Amount (USD): ");
+        double P = input.nextDouble();
+
+        System.out.print("Enter Annual Interest Rate (%): ");
+        double annualRate = input.nextDouble();
+
+        System.out.print("Enter Loan Duration (Years): ");
+        int years = input.nextInt();
+
+        double r = annualRate / 100 / 12; 
+        int n = years * 12; 
+
+        double monthlyPayment = P * (r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+
+        System.out.println("Monthly Mortgage Payment (USD): " + monthlyPayment);
+    }
+}
